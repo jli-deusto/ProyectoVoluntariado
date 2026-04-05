fda6bd16838211fef503d570dfaf1edc754f945c
jli-deusto <j.li@opendeusto.es>
2026-04-05 19:11:38 +0200

    correccion usuario/clave incorrecta

--- ProyectoVoluntariado/src/admin/validar/data_validar.c
+++ ProyectoVoluntariado/src/admin/validar/data_validar.c
@@ -25,10 +25,10 @@ int validarAdmin(char usuario[MAX_NOMBRE], char password[MAX_PASSWORD]) {
 		printf("Directorio actual: %s\n", cwd);
 		return 0;
 	} else if (strcmp(usuario, configAdmin.admin_user) != 0 || strcmp(password, configAdmin.admin_pass) != 0 ) {
-		printf("Admin user correcto: %s\n",configAdmin.admin_user);
-		printf("Admin pw correcto: %s\n",configAdmin.admin_pass);
-
-		puts("Usuario y/o clave incorrecta");
+//		printf("Admin user correcto: %s\n",configAdmin.admin_user);
+//		printf("Admin pw correcto: %s\n",configAdmin.admin_pass);
+//
+//		puts("Usuario y/o clave incorrecta");
 		return 0;
 	}
 
