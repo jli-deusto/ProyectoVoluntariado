801a321c5ed3f35b3ba80513cc5f56f141ebe689
teodoraalexandrab <teodoraalexandra.b@opendeusto.es>
2026-05-17 18:56:01 +0200

    modo admin // modo servidor

--- ProyectoVoluntariado/src/main.c
+++ ProyectoVoluntariado/src/main.c
@@ -14,42 +14,31 @@
 #include "Libs/sqlite/sqlite3.h"
 
 int main_server();
-
 int main() {
+    char cwd[256];
+    getcwd(cwd, sizeof(cwd));
+    printf("Directorio de trabajo: %s\n", cwd);
 
-	char cwd[256];
-	getcwd(cwd, sizeof(cwd));
-	printf("Directorio de trabajo: %s\n", cwd);
+    printf("1. Modo ADMINISTRADOR\n");
+    printf("2. Modo SERVIDOR\n");
+    printf("Seleccione una opcion: ");
+    fflush(stdout);
 
-    //int opcion;
-    main_server(); //carga de datos
-    logger_init();
-    while(1){
-    	bienvenida(); // Pagina inicial; datos admin
+    int opcion;
+    scanf("%d", &opcion);
+
+    if (opcion == 2) {
+        main_server();
+    } else {
+        logger_init();
+        while(1) {
+            bienvenida();
+        }
     }
-
-    //inicio_sesion(); // inicio sesion admin
 
     if (db != NULL) {
-    	sqlite3_close(db);
+        sqlite3_close(db);
     }
-
     return 0;
-
-//    printf("1. Ejecutar modo ADMIN\n");
-//    printf("2. Ejecutar modo SERVER\n");
-//    printf("Seleccione una opción: ");
-//	fflush(stdout);
-//    scanf("%d", &opcion);
-//
-//    if (opcion == 1) {
-//        return main_admin();
-//    } else if (opcion == 2) {
-//        return main_server();
-//    } else {
-//        printf("Opción no válida.\n");
-//        return 1;
-//    }
 }
-
 //prueba
